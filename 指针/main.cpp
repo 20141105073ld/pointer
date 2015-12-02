6e9aa67a9515d3de5be6fb640bb0b9aed05ccc53
20141105073ld <a20141105073@bieliangewifi.local>
2015-12-02 21:21:23 +0800

    Initial Commit

---
+++ 指针/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  指针
+//
+//  Created by 20141105073 on 15/12/2.
+//  Copyright © 2015年 20141105073. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
